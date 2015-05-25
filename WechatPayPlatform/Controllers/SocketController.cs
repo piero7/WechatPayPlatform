66e6fa2e747c6ed43762b8be98de7867cc52a508
piero7 <piero_chen@163.com>
2015-05-25 17:19:45 +0800

    add machine model

---
+++ WechatPayPlatform/Controllers/SocketController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WechatPayPlatform.Controllers
+{
+    public class SocketController : ApiController
+    {
+        [HttpGet]
+        public bool GetMachine(string machineId)
+        {
+            throw new NotImplementedException();
+            return false;
+        }
+    }
+}
---
+++ WechatPayPlatform/Models/Machine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WechatPayPlatform.Models
+{
+    class Machine
+    {
+        [Key]
+        public int MachineId { get; set; }
+
+        public string Name { get; set; }
+
+        public string MachineNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string Remarks { get; set; }
+
+        public MachineType Type { get; set; }
+
+    }
+
+    public enum MachineType
+    {
+        Unknown = 0,
+        CarCleaner = 1,
+    }
+}
