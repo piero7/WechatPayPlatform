f801132c387781a07826bbb1c0dff211db746c7b
piero7 <piero_chen@163.com>
2015-07-01 14:09:05 +0800

    finish version 1

---
+++ WechatPayPlatform/Controllers/BillInfoController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WechatPayPlatform.Models;
+
+namespace WechatPayPlatform.Controllers
+{
+    public class BillInfoController : Controller
+    {
+        //
+        // GET: /BillInfo/
+
+        public ActionResult Index(string code)
+        {
+            if (string.IsNullOrEmpty(code))
+            {
+                //TODO: code 为空
+
+                //debug
+                // return RedirectToAction("IndexWithOpenid", "BillInfo", (object)"testtest");
+            }
+            var helper = new PayInfoController();
+            var openid = helper.GetOpenidByCode(code);
+
+            return RedirectToAction("IndexWithOpenid", "BillInfo", new { openid = openid });
+
+        }
+
+        public ActionResult IndexWithOpenid(string openid)
+        {
+            if (string.IsNullOrEmpty(openid))
+            {
+                return RedirectToAction("Fail", "ComeBill");
+            }
+            var db = new ModelContext();
+            var user = db.WechatUserSet.FirstOrDefault(item => item.OpenId == openid);
+            if (user == null)
+            {
+                //TODO: 没找到user 
+
+                //debug
+                // user = db.WechatUserSet.FirstOrDefault(item => item.OpenId == "testtest");
+                user = new WechatUser
+                {
+                    OpenId = openid,
+                    SubscribeTime = DateTime.Now,
+                    Balance = 0,
+                    subscribe = true,
+                    Sex = 0
+                };
+                db.WechatUserSet.Add(user);
+                db.SaveChanges();
+
+                Helper.GetUserInfo(user);
+            }
+
+            var dic = new Dictionary<string, IEnumerable<Bill>>();
+            if (!db.ComeBillSet.Any(item => item.UserId == user.UserId))
+            {
+                dic.Add("come", new List<ComeBill>());
+            }
+            else
+            {
+                dic.Add("come", db.ComeBillSet.Where(item => item.UserId == user.UserId).OrderByDescending(item => item.CreateDate).AsEnumerable());
+            }
+
+            if (!db.MachineBillSet.Any(item => item.UserId == user.UserId))
+            {
+                dic.Add("machine", new List<MachineBill>());
+            }
+            else
+            {
+                dic.Add("machine", db.MachineBillSet.Where(item => item.UserId == user.UserId).OrderByDescending(item => item.CreateDate).AsEnumerable());
+            }
+
+            if (!db.ShopBillSet.Any(item => item.UserId == user.UserId))
+            {
+                dic.Add("shop", new List<ShopBill>());
+            }
+            else
+            {
+                dic.Add("shop", db.ShopBillSet.Where(item => item.UserId == user.UserId).OrderByDescending(item => item.CreateDate).AsEnumerable());
+            }
+
+            //dic.Add("recharge", db.RechargeBillSet);
+            //return View("BillInfo/Inedx", dic);
+            return View(dic);
+        }
+
+        [HttpGet]
+        public ActionResult WorkerBillInfo(int? adminid)
+        {
+            var db = new ModelContext();
+            var dic = new Dictionary<string, IEnumerable<ComeBill>>();
+
+            //TODO: check adminid
+
+
+            dic.Add("confirm", db.ComeBillSet.Where(item => item.AdminId == adminid
+                && item.Status == ComeBillStatus.ToConfirm)
+                .OrderByDescending(item => item.CreateDate)
+                .AsEnumerable());
+            dic.Add("working", db.ComeBillSet.Where(item => item.AdminId == adminid
+                && item.Status == ComeBillStatus.Working)
+                .OrderByDescending(item => item.CreateDate)
+                .AsEnumerable());
+            dic.Add("finish", db.ComeBillSet.Where(item => item.AdminId == adminid
+                && item.Status != ComeBillStatus.ToConfirm
+                && item.Status != ComeBillStatus.Working)
+                .OrderByDescending(item => item.CreateDate)
+                .AsEnumerable());
+            ViewData.Add("adminid", adminid);
+            return View(dic);
+        }
+
+        [HttpGet]
+        public ActionResult GetWorkInfo(int? adminid)
+        {
+            var db = new ModelContext();
+            ViewBag.usercount = db.ComeBillSet.Where(item => item.Status == ComeBillStatus.Finish).Select(item => item.UserId).Distinct().Count();
+            // = Helper.GetUserCount() - 10;
+            ViewBag.tocom = db.ComeBillSet.Count(item => item.Status == ComeBillStatus.ToConfirm);
+            ViewBag.todo = db.ComeBillSet.Count(item => item.Status == ComeBillStatus.Working);
+            ViewBag.topay = db.ComeBillSet.Count(item => item.Status == ComeBillStatus.ToPay);
+            ViewBag.finish = db.ComeBillSet.Count(item => item.Status == ComeBillStatus.Finish);
+
+            return View();
+        }
+
+    }
+}
+
---
+++ WechatPayPlatform/Controllers/ComeHelpController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace WechatPayPlatform.Controllers
+{
+    public class ComeHelpController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<object> GetStationList()
+        {
+            var db = new Models.ModelContext();
+            return db.StationSet.Select(item => new
+            {
+                name = item.Name,
+                id = item.StationId
+            }).AsEnumerable();
+        }
+
+        [HttpGet]
+        public object GetUserInfo(string openid)
+        {
+            var db = new Models.ModelContext();
+            // var helper = new PayInfoController();
+            //var openid = helper.GetOpenidByCode(code);
+            var user = db.WechatUserSet.FirstOrDefault(item => item.OpenId == openid);
+            if (user == null)
+            {
+                return new { openid = openid };
+            }
+            var car = db.CarInfoSet.Where(item => item.UserId == user.UserId).OrderByDescending(item => item.LastUseDate).FirstOrDefault();
+            if (car == null)
+            {
+                return new
+                {
+                    openid = openid,
+                    phone = user.PhoneNumber
+                };
+            }
+            return new
+            {
+                openid = openid,
+                phone = user.PhoneNumber,
+                sid = car.LocationDescribe,
+                car = car.CarNumber,
+                describe = car.Describe
+            };
+        }
+
+
+        [HttpPost]
+        public string AddComeOrder(string openid, string location, string carnumber, DateTime endtime, string phone, string desc, string station)
+        {
+            return "";
+        }
+
+        [HttpPost]
+        public string GetJsSign([FromBody]string url, string str, string time)
+        {
+            var sign = Helper.GetJsApiSignature(url, str, time);
+            return sign;
+        }
+    }
+}
